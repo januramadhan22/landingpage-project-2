c21d48bbb36d40e18295fc96c0a3362772cdae5d
Janu <abbyjunior600@gmail.com>
2023-02-15 10:22:05 +0700

    slicing services

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ServicesItem from "./card/ServicesItem";
+
+function Services() {
+  return (
+    <div className="w-full py-16 px-24 grid grid-cols-3 justify-center gap-10">
+      <ServicesItem title={"Our Service"} isDesc />
+      <ServicesItem title={"Management Investment"} value={1} />
+      <ServicesItem title={"Financial Report"} value={2} />
+      <ServicesItem title={"Audit & Assurance"} value={3} />
+      <ServicesItem title={"Financial Plan"} value={4} />
+      <ServicesItem title={"Profit Planning"} value={5} />
+    </div>
+  );
+}
+
+export default Services;
