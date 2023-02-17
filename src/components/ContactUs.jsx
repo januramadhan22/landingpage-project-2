7a6482d10b7aa849a761b5bc2359efeabb5e422f
Janu <abbyjunior600@gmail.com>
2023-02-17 18:46:48 +0700

    add responsive to 1180

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ContactUs() {
   return (
-    <div className="w-full h-96 bg-[url('https://plus.unsplash.com/premium_photo-1661573644696-d670a189244b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGN1c3RvbWVyJTIwc2VydmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')] bg-no-repeat bg-top bg-cover relative flex items-center justify-center">
+    <div className="w-full md:h-80 lg:h-96 bg-[url('https://plus.unsplash.com/premium_photo-1661573644696-d670a189244b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGN1c3RvbWVyJTIwc2VydmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')] bg-no-repeat bg-top bg-cover relative flex items-center justify-center">
       <div className="z-20 relative max-w-lg text-center space-y-5">
         <h1 className="text-4xl font-bold text-white">
           Bring A New Experience To Your Investment
